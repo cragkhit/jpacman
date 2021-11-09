dfe75f52feb3f788797368c931b738184e08c07d
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2021-11-09 11:16:13 +0700

    Update

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -7,13 +7,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
 
+/**
+ * This is a test class for MapParser.
+ */
 @ExtendWith(MockitoExtension.class)
 public class MapParserTest {
     @Mock
@@ -23,23 +26,30 @@ public class MapParserTest {
     @Mock
     private Blinky blinky;
 
+    /**
+     * Test for the parseMap method (good map).
+     */
     @Test
     public void testParseMapGood() {
         MockitoAnnotations.initMocks(this);
         assertNotNull(boardFactory);
         assertNotNull(levelFactory);
-        when(levelFactory.createGhost()).thenReturn(blinky);
+        Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
         MapParser mapParser = new MapParser(levelFactory, boardFactory);
         ArrayList<String> map = new ArrayList<>();
         map.add("############");
         map.add("#P        G#");
         map.add("############");
         mapParser.parseMap(map);
-        verify(levelFactory, times(1)).createGhost();
-        verify(boardFactory, times(1)).createBoard(any());
-        verify(levelFactory, times(1)).createLevel(any(), any(), any());
+        Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
+        Mockito.verify(boardFactory, Mockito.times(1)).createBoard(Mockito.any());
+        Mockito.verify(levelFactory, Mockito.times(1))
+            .createLevel(Mockito.any(), Mockito.any(), Mockito.any());
     }
 
+    /**
+     * Test for the parseMap method (bad map).
+     */
     @Test
     public void testParseMapWrong1() {
         PacmanConfigurationException thrown = Assertions.assertThrows(PacmanConfigurationException.class, () -> {
